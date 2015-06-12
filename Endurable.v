187874a55aefac7b6b8e3208a56273e38fe2cb23
Koleman Nix <kolemannix@gmail.com>
2015-06-12 16:45:49 -0400

    remove old ility files

--- Endurable.v
+++
@@ -1,6 +0,0 @@
-(** Endurable General Theory *)
-Require Export System.
-
-Inductive Endurable {msys: MetaSystem} (sys: System msys) : Prop :=
-  satisfiesEndurabilityRequirement:
-    (exists endurable: System msys -> Prop, endurable sys) -> Endurable sys.
