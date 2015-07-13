eb09ec3a4c7b347812e9189a1564216e8201389f
Ke-Dou <kd6ck@virginia.edu>
2015-07-13 16:15:01 -0400

    Updated Parnas Information Hiding Example

---
+++ Example_Parnas_Information_Hiding.v
@@ -0,0 +1,173 @@
+Require Import DesignStructure.
+Import ListNotations.
+Open Scope string_scope.
+Require Export System.
+Require Export Changeable.
+
+(** ** Context *)
+
+Inductive kwicContexts := nominal.
+
+(** ** Phase *)
+
+(**
+[kwicPhases] represents the lifecycle phases of a software system.
+*)
+
+(** design, implementation and maintenance are talked about in the paper*)
+Inductive kwicPhases := design | implementation | maintenance.
+
+(** ** Stakeholder *)
+
+(**
+[kwicStakeholders] represents the set of potential system change agents
+*)
+
+Inductive kwicStakeholders :=  developer | customer.
+
+(** ** Value for measuring cost-benefit *)
+
+(** 
+[kwicValue] is a framework for quantifying time, money, and other
+elements of value that can be gained or lost as a result of a change.
+*)
+
+Inductive kwicValue := mk_value { 
+  modulesChanged: nat
+}.
+
+Inductive computerState := computer_pre | computer_post.
+Inductive corpusState := corpus_pre | corpus_post.
+Inductive userState := user_pre | user_post.
+
+Record kwicVolatileState : Set := {
+  computer_state: computerState;
+  corpus_state: corpusState;
+  user_state: userState
+}.
+
+
+Inductive kwicParameter := computer | corpus | user
+                             | line_abs | line_alg | line_data
+                             | alph_abs | alph_alg | alph_data
+                             | circ_abs | circ_alg | circ_data
+                             | input_abs | input_alg | input_data
+                             | output_abs | output_alg | output_data
+                             | master.
+
+Definition uses (a b: kwicParameter): Prop :=
+    match a, b with
+      | master, (line_abs | input_abs | circ_abs | alph_abs | output_abs) => True
+      | line_alg, line_data => True
+      | line_data, line_alg  => True
+      | input_alg, (input_data | line_abs) => True
+      | input_data, input_alg  => True
+      | circ_alg, (circ_data  | line_abs) => True
+      | circ_data, (circ_alg  | line_abs) => True
+      | alph_alg, (alph_data | line_abs) => True
+      | alph_data, (alph_alg | line_abs)  => True
+      | output_alg, (output_data | line_abs)  => True
+      | output_data, output_alg  => True
+      | _, _ => False
+    end.
+
+  Definition satisfies (a b: kwicParameter): Prop :=
+    match b, a with
+      | computer,  (line_data | line_alg | input_data | circ_data | alph_data | alph_alg | output_data | output_alg)=> True
+      | corpus,  (line_data | line_alg | input_data | input_alg | circ_data | circ_alg | alph_data | alph_alg | output_data | output_alg)=> True
+      | user, (input_alg | circ_alg | alph_alg | master) => True
+      | line_abs, (line_data | line_alg) => True
+      | input_abs, (input_data | input_alg) => True
+      | circ_abs, (circ_data | circ_alg) => True
+      | alph_abs, (alph_data | alph_alg) => True
+      | output_abs, (output_data | output_alg) => True
+      | _, _ => False
+    end.
+
+Definition input_mod := {| name := "Input";
+      elements := [input_data; input_alg; input_abs]|}.
+Definition line_mod := {| name := "Line";
+       elements := [line_data; line_alg; line_abs]|}.
+Definition circ_mod := {| name := "Circular";
+       elements := [circ_data; circ_alg; circ_abs]|}.
+Definition alph_mod := {| name := "Alphabetizer";
+       elements := [alph_data; alph_alg; alph_abs]|}.
+Definition out_mod := {| name := "Output";
+       elements := [output_data; output_alg; output_abs]|}.
+Definition master_mod :=  {| name := "Master";
+       elements := [master]|}.
+Definition env_mod := {| name := "Environment";
+       elements := [user; computer; corpus]|}.
+
+Definition kwic_modules : list (@Module kwicParameter) :=
+  [input_mod; line_mod; circ_mod; alph_mod; out_mod; master_mod; env_mod].
+
+Definition kwic_volatile (p : kwicParameter): Prop :=
+  match p with
+    | corpus => True
+    | computer => True
+    | user => True
+    | _ => False
+  end.
+
+Definition kwic_ds: DesignStructure := {|
+                                 Modules := kwic_modules;
+                                 Volatile := kwic_volatile;
+                                 Uses:= uses;
+                                 Satisfies:= satisfies
+                               |}.
+
+Inductive kwic_ds_type := 
+    | mk_kwic_ds : forall ds: DesignStructure, ds = kwic_ds -> kwic_ds_type.
+
+Definition extract_kwic_ds kdt :=
+  match kdt with
+    | mk_kwic_ds ds _ => ds
+  end.
+
+Inductive kwic := mk_kwic {
+  kwic_volatile_state: kwicVolatileState;
+  kwic_design: kwic_ds_type
+}.
+
+Definition kwicSystemType := mk_system_type kwicContexts kwicStakeholders kwicPhases kwic kwicValue.
+
+(**
+Abbreviations for writing propositions, assertions, actions.
+*)
+
+Definition kwicSystemState := @SystemInstance kwicSystemType.
+
+Definition kwicAssertion := @Assertion kwicSystemType.
+
+Definition kwicAction := @Action kwicSystemType.
+
+(*test more specifically whether a system is modular with respect to a single parameter*)
+Definition satisfiesModularity_wrt (kp: kwicParameter) (ks: kwicSystemState): Prop := modular_wrt kp (extract_kwic_ds (kwic_design (artifact ks))).
+
+Definition computerPre (ks: kwicSystemState): Prop := computer_state (kwic_volatile_state (artifact ks)) = computer_pre.
+Definition computerPost (ks: kwicSystemState): Prop := computer_state (kwic_volatile_state (artifact ks)) = computer_post.
+Definition corpusPre (ks: kwicSystemState): Prop := corpus_state (kwic_volatile_state (artifact ks)) = corpus_pre.
+Definition corpusPost (ks: kwicSystemState): Prop := corpus_state (kwic_volatile_state (artifact ks)) = corpus_post.
+Definition userPre (ks: kwicSystemState): Prop := user_state (kwic_volatile_state (artifact ks)) = user_pre.
+Definition userPost (ks: kwicSystemState): Prop := user_state (kwic_volatile_state (artifact ks)) = user_post.
+Definition inMaintenancePhase (ks: kwicSystemState): Prop := phase ks = maintenance.
+
+Definition computerPreState : kwicAssertion := fun ks: kwicSystemState => computerPre ks.
+Definition computerPostState : kwicAssertion := fun ks: kwicSystemState => computerPost ks.
+Definition corpusPreState : kwicAssertion := fun ks: kwicSystemState => corpusPre ks.
+Definition corpusPostState : kwicAssertion := fun ks: kwicSystemState => corpusPost ks.
+Definition userPreState : kwicAssertion := fun ks: kwicSystemState => userPre ks.
+Definition userPostState : kwicAssertion := fun ks: kwicSystemState => userPost ks.
+
+Definition costomerChangeCorpus: kwicAction :=
+  fun ks: kwicSystemState =>
+      mk_system kwicSystemType
+                         nominal
+                         maintenance
+                         {| kwic_volatile_state := {|  computer_state := computer_state (kwic_volatile_state (artifact ks));
+                                                                   corpus_state:= corpus_pre;
+                                                                   user_state:= user_state (kwic_volatile_state (artifact ks))
+                                                               |};
+                            kwic_design := mk_kwic_ds kwic_ds eq_refl |}
+                          (value ks).
---
+++ Example_Parnas_Information_Hiding_Changeable.v
@@ -0,0 +1,289 @@
+Require Export Quality.
+Require Export Value.
+Require Export Example_Parnas_Information_Hiding.
+Require Export BoehmTactics.
+
+Definition corpusChangeActionSpec (trigger: kwicAssertion) (agent: kwicStakeholders) (pre post: kwicSystemState): Prop  :=  
+        agent = customer /\ corpusPre pre /\ inMaintenancePhase pre ->
+        corpusPost post /\ satisfiesModularity_wrt corpus pre.
+
+Theorem verifyChangeCorpus: ActionSatisfiesActionSpec (corpusChangeActionSpec corpusPre customer) costomerChangeCorpus.
+Proof.
+Admitted.
+
+Definition kwic_changeability_reqs (c: kwicContexts) (p: kwicPhases) (s: kwicStakeholders) (st: kwicSystemState): Prop :=
+  match c, p, s, st with
+       | nominal, maintenance, customer, _ =>  ActionSatisfiesActionSpec (corpusChangeActionSpec corpusPre customer) costomerChangeCorpus
+       | _, _, _, _ => True
+  end.
+
+Definition kwic_accuracy_reqs (c: kwicContexts) (p: kwicPhases) (s: kwicStakeholders) (st: kwicSystemState): Prop := True.
+Definition kwic_physicalCapability_reqs (c: kwicContexts) (p: kwicPhases) (s: kwicStakeholders) (st: kwicSystemState): Prop := True.
+Definition kwic_cyberCapability_reqs (c: kwicContexts) (p: kwicPhases) (s: kwicStakeholders) (st: kwicSystemState): Prop := True.
+Definition kwic_humanUsability_reqs (c: kwicContexts) (p: kwicPhases) (s: kwicStakeholders) (st: kwicSystemState): Prop := True.
+Definition kwic_speed_reqs (c: kwicContexts) (p: kwicPhases) (s: kwicStakeholders) (st: kwicSystemState): Prop := True.
+Definition kwic_endurability_reqs (c: kwicContexts) (p: kwicPhases) (s: kwicStakeholders) (st: kwicSystemState): Prop := True.
+Definition kwic_maneuverability_reqs (c: kwicContexts) (p: kwicPhases) (s: kwicStakeholders) (st: kwicSystemState): Prop := True.
+Definition kwic_impact_reqs (c: kwicContexts) (p: kwicPhases) (s: kwicStakeholders) (st: kwicSystemState): Prop := True.
+Definition kwic_scalability_reqs (c: kwicContexts) (p: kwicPhases) (s: kwicStakeholders) (st: kwicSystemState): Prop := True.
+Definition kwic_versatility_reqs (c: kwicContexts) (p: kwicPhases) (s: kwicStakeholders) (st: kwicSystemState): Prop := True.
+Definition kwic_interoperability_reqs (c: kwicContexts) (p: kwicPhases) (s: kwicStakeholders) (st: kwicSystemState): Prop := True.
+Definition kwic_missionEffectiveness_reqs (c: kwicContexts) (p: kwicPhases) (s: kwicStakeholders) (st: kwicSystemState): Prop := True.
+
+Definition kwic_cost_reqs (c: kwicContexts) (p: kwicPhases) (s: kwicStakeholders) (st: kwicSystemState): Prop := True.
+Definition kwic_duration_reqs (c: kwicContexts) (p: kwicPhases) (s: kwicStakeholders) (st: kwicSystemState): Prop := True.
+Definition kwic_keyPersonnel_reqs (c: kwicContexts) (p: kwicPhases) (s: kwicStakeholders) (st: kwicSystemState): Prop := True.
+Definition kwic_otherScarceResources_reqs (c: kwicContexts) (p: kwicPhases) (s: kwicStakeholders) (st: kwicSystemState): Prop := True.
+Definition kwic_manufacturability_reqs (c: kwicContexts) (p: kwicPhases) (s: kwicStakeholders) (st: kwicSystemState): Prop := True.
+Definition kwic_sustainability_reqs (c: kwicContexts) (p: kwicPhases) (s: kwicStakeholders) (st: kwicSystemState): Prop := True.
+Definition kwic_efficiency_reqs (c: kwicContexts) (p: kwicPhases) (s: kwicStakeholders) (st: kwicSystemState): Prop := True.
+
+Definition kwic_affordability_reqs (c: kwicContexts) (p: kwicPhases) (s: kwicStakeholders) (st: kwicSystemState): Prop := True.
+
+Definition kwic_security_reqs (c: kwicContexts) (p: kwicPhases) (s: kwicStakeholders) (st: kwicSystemState): Prop := True.
+Definition kwic_safety_reqs (c: kwicContexts) (p: kwicPhases) (s: kwicStakeholders) (st: kwicSystemState): Prop := True.
+Definition kwic_reliability_reqs (c: kwicContexts) (p: kwicPhases) (s: kwicStakeholders) (st: kwicSystemState): Prop := True.
+Definition kwic_maintainability_reqs (c: kwicContexts) (p: kwicPhases) (s: kwicStakeholders) (st: kwicSystemState): Prop := True.
+Definition kwic_availability_reqs (c: kwicContexts) (p: kwicPhases) (s: kwicStakeholders) (st: kwicSystemState): Prop := True.
+Definition kwic_survivability_reqs (c: kwicContexts) (p: kwicPhases) (s: kwicStakeholders) (st: kwicSystemState): Prop := True.
+Definition kwic_robustness_reqs (c: kwicContexts) (p: kwicPhases) (s: kwicStakeholders) (st: kwicSystemState): Prop := True.
+Definition kwic_dependability_reqs (c: kwicContexts) (p: kwicPhases) (s: kwicStakeholders) (st: kwicSystemState): Prop := True.
+
+Definition kwic_modifiability_reqs (c: kwicContexts) (p: kwicPhases) (s: kwicStakeholders) (st: kwicSystemState): Prop := True.
+Definition kwic_tailorability_reqs (c: kwicContexts) (p: kwicPhases) (s: kwicStakeholders) (st: kwicSystemState): Prop := True.
+Definition kwic_adaptability_reqs (c: kwicContexts) (p: kwicPhases) (s: kwicStakeholders) (st: kwicSystemState): Prop := True.
+Definition kwic_flexibility_reqs (c: kwicContexts) (p: kwicPhases) (s: kwicStakeholders) (st: kwicSystemState): Prop := True.
+
+Definition kwic_resiliency_reqs (c: kwicContexts) (p: kwicPhases) (s: kwicStakeholders) (st: kwicSystemState): Prop := True.
+
+Definition kwic_satisfactory_reqs (c: kwicContexts) (p: kwicPhases) (s: kwicStakeholders) (st: kwicSystemState): Prop := True.
+
+Hint Unfold kwic_changeability_reqs kwic_accuracy_reqs kwic_physicalCapability_reqs kwic_cyberCapability_reqs kwic_humanUsability_reqs
+            kwic_speed_reqs kwic_endurability_reqs kwic_maneuverability_reqs kwic_impact_reqs kwic_scalability_reqs kwic_versatility_reqs
+            kwic_interoperability_reqs kwic_missionEffectiveness_reqs kwic_cost_reqs kwic_duration_reqs kwic_keyPersonnel_reqs
+            kwic_otherScarceResources_reqs kwic_manufacturability_reqs kwic_sustainability_reqs kwic_efficiency_reqs kwic_affordability_reqs
+            kwic_security_reqs kwic_safety_reqs kwic_reliability_reqs kwic_maintainability_reqs kwic_availability_reqs kwic_survivability_reqs
+            kwic_robustness_reqs kwic_dependability_reqs kwic_modifiability_reqs kwic_tailorability_reqs kwic_adaptability_reqs kwic_flexibility_reqs
+            kwic_resiliency_reqs kwic_satisfactory_reqs.
+
+(**
+Proofs for satisfying ility requirements.
+*)
+
+Theorem kwic_changeability_certificate: @Changeable kwicSystemType.
+Proof.
+Prove_satisfaction kwic_changeability_reqs c p s
+;simpl; exact verifyChangeCorpus.
+Qed.
+
+Theorem kwic_accuracy_certificate: @Accurate kwicSystemType.
+Proof.
+Prove_satisfaction kwic_accuracy_reqs c p s.
+Qed.
+
+Theorem kwic_physicalCapability_certificate: @PhysicalCapable kwicSystemType.
+Proof.
+Prove_satisfaction kwic_physicalCapability_reqs c p s.     
+Qed.
+
+Theorem kwic_cyberCapability_certificate: @CyberCapable kwicSystemType.
+Proof.
+Prove_satisfaction kwic_cyberCapability_reqs c p s.    
+Qed.
+
+Theorem kwic_humanUsability_certificate: @HumanUsable kwicSystemType.
+Proof.
+Prove_satisfaction kwic_humanUsability_reqs c p s.  
+Qed.
+
+Theorem kwic_speed_certificate: @Speed kwicSystemType.
+Proof.
+Prove_satisfaction kwic_speed_reqs c p s. 
+Qed.
+
+Theorem kwic_endurability_certificate: @Endurable kwicSystemType.
+Proof.
+Prove_satisfaction kwic_endurability_reqs c p s.    
+Qed.
+
+Theorem kwic_maneuverability_certificate: @Maneuverable kwicSystemType.
+Proof.
+Prove_satisfaction kwic_maneuverability_reqs c p s.   
+Qed.
+
+Theorem kwic_impact_certificate: @Impactful kwicSystemType.
+Proof.
+Prove_satisfaction kwic_impact_reqs c p s. 
+Qed.
+
+Theorem kwic_scalability_certificate: @Scalable kwicSystemType.
+Proof.
+Prove_satisfaction kwic_scalability_reqs c p s.
+Qed.
+
+Theorem kwic_versatility_certificate: @Versatile kwicSystemType.
+Proof.
+Prove_satisfaction kwic_versatility_reqs c p s. 
+Qed.
+
+Theorem kwic_interoperability_certificate: @Interoperable kwicSystemType.
+Proof.
+Prove_satisfaction kwic_interoperability_reqs c p s.      
+Qed.
+
+Theorem kwic_cost_certificate: @Cost kwicSystemType.
+Proof.
+Prove_satisfaction kwic_cost_reqs c p s.  
+Qed.
+
+Theorem kwic_duration_certificate: @Duration kwicSystemType.
+Proof.
+Prove_satisfaction kwic_duration_reqs c p s.  
+Qed.
+
+Theorem kwic_keyPersonnel_certificate: @KeyPersonnel kwicSystemType.
+Proof.
+Prove_satisfaction kwic_keyPersonnel_reqs c p s.   
+Qed.
+
+Theorem kwic_otherScarceResources_certificate: @OtherScarceResources kwicSystemType.
+Proof.
+Prove_satisfaction kwic_otherScarceResources_reqs c p s.    
+Qed.
+
+Theorem kwic_manufacturability_certificate: @Manufacturable kwicSystemType.
+Proof.
+Prove_satisfaction kwic_manufacturability_reqs c p s.  
+Qed.
+
+Theorem kwic_sustainability_certificate: @Sustainable kwicSystemType.
+Proof.
+Prove_satisfaction kwic_sustainability_reqs c p s.    
+Qed.
+
+Theorem kwic_security_certificate: @Secure kwicSystemType.
+Proof.
+Prove_satisfaction kwic_security_reqs c p s.   
+Qed.
+
+Theorem kwic_safety_certificate: @Safe kwicSystemType.
+Proof.
+Prove_satisfaction kwic_safety_reqs c p s.  
+Qed.
+
+Theorem kwic_reliability_certificate: @Reliable kwicSystemType.
+Proof.
+Prove_satisfaction kwic_reliability_reqs c p s.  
+Qed.
+
+Theorem kwic_maintainability_certificate: @Maintainable kwicSystemType.
+Proof.
+Prove_satisfaction kwic_maintainability_reqs c p s.  
+Qed.
+
+Theorem kwic_availability_certificate: @Available kwicSystemType.
+Proof.
+Prove_satisfaction kwic_availability_reqs c p s.
+Qed.
+
+Theorem kwic_survivability_certificate: @Survivable kwicSystemType.
+Proof.
+Prove_satisfaction kwic_survivability_reqs c p s.  
+Qed.
+
+Theorem kwic_robustness_certificate: @Robust kwicSystemType.
+Proof.
+Prove_satisfaction kwic_robustness_reqs c p s.   
+Qed.
+
+Theorem kwic_modifiability_certificate: @Modifiable kwicSystemType.
+Proof.
+Prove_satisfaction kwic_modifiability_reqs c p s.   
+Qed.
+
+Theorem kwic_tailorability_certificate: @Tailorable kwicSystemType.
+Proof.
+Prove_satisfaction kwic_tailorability_reqs c p s.   
+Qed.
+
+Theorem kwic_adaptability_certificate: @Adaptable kwicSystemType.
+Proof.
+Prove_satisfaction kwic_adaptability_reqs c p s.   
+Qed.
+
+Hint Resolve kwic_changeability_certificate kwic_physicalCapability_certificate kwic_cyberCapability_certificate kwic_humanUsability_certificate
+             kwic_speed_certificate kwic_endurability_certificate kwic_maneuverability_certificate kwic_accuracy_certificate 
+             kwic_impact_certificate kwic_scalability_certificate kwic_versatility_certificate kwic_interoperability_certificate
+             kwic_cost_certificate kwic_duration_certificate kwic_keyPersonnel_certificate kwic_otherScarceResources_certificate
+             kwic_manufacturability_certificate kwic_sustainability_certificate kwic_security_certificate kwic_safety_certificate 
+             kwic_reliability_certificate kwic_maintainability_certificate kwic_availability_certificate kwic_survivability_certificate
+             kwic_robustness_certificate kwic_modifiability_certificate kwic_tailorability_certificate kwic_adaptability_certificate.
+
+Theorem kwic_missionEffectiveness_certificate: @MissionEffective kwicSystemType.
+Proof.
+auto.
+Qed.
+
+Theorem kwic_efficiency_certificate: @Efficient kwicSystemType.
+Proof.
+auto.
+Qed.
+
+Theorem kwic_flexibility_certificate: @Flexible kwicSystemType.
+Proof.
+auto.
+Qed.
+
+Theorem kwic_dependability_certificate: @Dependable kwicSystemType.
+Proof.
+auto.
+Qed.
+
+Theorem kwic_affordability_certificate: @Affordable kwicSystemType.
+Proof.
+auto.
+Qed.
+
+Theorem kwic_resiliency_certificate: @Resilient kwicSystemType.
+Proof.
+auto.
+Qed.
+
+Theorem kwic_satisfactory_certificate: @Satisfactory kwicSystemType.
+Proof.
+auto.
+Qed.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
