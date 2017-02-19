f4d211408ae89c67d5f5a336f5df780c01db6d7d
Kevin Sullivan <sullivan.kevinj@gmail.com>
2017-02-18 22:48:07 -0500

    Refactoring within Lean version of the project.

--- Lean/SystemMetaModel/Value.lean
+++ Lean/SystemMetaModel/Value.lean
@@ -4,3 +4,7 @@ import System.
 definition valueType {sys_type : SystemType} := sys_type^.ValueType
 record Value {sys_type : SystemType} := 
 mk :: (value_cost: (@valueType sys_type)) (value_benefit: (@valueType sys_type))
+
+/-
+ - Note that what we really need here is a more sophisticated accounting system.
+ -/
